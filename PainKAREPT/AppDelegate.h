935825c719e828f23c6a187ee23b433b681d8dbc
Honey <Honey@honeymac.local>
2016-11-15 10:17:30 +0800

    Initial Commit

---
+++ PainKAREPT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PainKAREPT
+//
+//  Created by Honey on 16/11/15.
+//  Copyright © 2016年 Honey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PainKAREPT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PainKAREPT
+//
+//  Created by Honey on 16/11/15.
+//  Copyright © 2016年 Honey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
